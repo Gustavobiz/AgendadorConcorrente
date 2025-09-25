8b33db3c0058004b96b67887fd0dc71f8c9b8b28
Gustavobiz <gustavobernardes05@hotmail.com>
2025-09-25 19:34:25 -0300

    feat: Construtor atualizado

--- Tarefa.java
+++ Tarefa.java
@@ -2,16 +2,26 @@ public class Tarefa implements Runnable {
 
     private final String nome;
     private final PoolDeRecursos pool;
+    private final TipoTarefa tipo;
 
-    public Tarefa(String nome, PoolDeRecursos pool) {
+    public Tarefa(String nome, PoolDeRecursos pool, TipoTarefa tipo) {
         this.nome = nome;
         this.pool = pool;
+        this.tipo = tipo; 
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public TipoTarefa getTipo() {
+        return tipo; 
     }
 
     @Override
     public void run() {
         try {
-            pool.acessarRecurso(this.nome);
+            pool.acessarRecurso(this); 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             System.out.println("Tarefa " + nome + " foi interrompida.");
