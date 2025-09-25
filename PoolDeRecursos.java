4e1125fe4cde02dc2f43a3b62ed47e7c87cc291b
Gustavobiz <gustavobernardes05@hotmail.com>
2025-09-25 19:20:36 -0300

    commentario

--- PoolDeRecursos.java
+++ PoolDeRecursos.java
@@ -9,12 +9,12 @@ public class PoolDeRecursos {
     private final int numeroDeRecursos;
     private final Semaphore semaphore;
     private final List<Recurso> recursos;
-    private final Lock lockPool = new ReentrantLock(); // Lock para proteger a lista de recursos
+    private final Lock lockPool = new ReentrantLock(); 
 
     public PoolDeRecursos(int numeroDeRecursos) {
         this.numeroDeRecursos = numeroDeRecursos;
-        // O semáforo começa com N "permits", um para cada recurso
-        this.semaphore = new Semaphore(numeroDeRecursos, true); // 'true' para modo justo
+       
+        this.semaphore = new Semaphore(numeroDeRecursos, true); 
         this.recursos = new ArrayList<>();
         for (int i = 1; i <= numeroDeRecursos; i++) {
             recursos.add(new Recurso(i));
@@ -22,18 +22,16 @@ public class PoolDeRecursos {
     }
 
     public void acessarRecurso(String nomeDaTarefa) throws InterruptedException {
-        System.out.println(nomeDaTarefa + " está na fila, tentando adquirir um permit do semáforo...");
+        System.out.println(nomeDaTarefa + "Esta na fila tentando adquirir um permit do semáforo...");
 
-        // 1. Tenta adquirir um "permit". Se o semáforo estiver com 0 permits,
-        // a thread ficará bloqueada aqui esperando a liberação.
+
         semaphore.acquire();
 
         Recurso recursoParaUsar = null;
         try {
             System.out.println("-> " + nomeDaTarefa + " adquiriu um permit!");
             
-            // 2. Procura por um recurso livre na lista.
-            // A lista é um recurso compartilhado, então precisamos de um lock para protegê-la.
+ 
             lockPool.lock();
             for (Recurso r : recursos) {
                 if (r.estaLivre()) {
@@ -43,14 +41,13 @@ public class PoolDeRecursos {
             }
             lockPool.unlock();
 
-            // 3. Usa o recurso encontrado
+
             if (recursoParaUsar != null) {
                 recursoParaUsar.usar(nomeDaTarefa);
             }
 
         } finally {
-            // 4. Libera o "permit" de volta para o semáforo, permitindo
-            // que outra thread que esteja esperando possa entrar.
+
             semaphore.release();
             System.out.println("<- " + nomeDaTarefa + " liberou seu permit.");
         }
